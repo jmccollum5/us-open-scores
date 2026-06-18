77852d0e33f8d85e0807e6c776f0f2fd3d1be203
jmccollum5 <jackwmccollum@gmail.com>
2026-06-18 07:47:23 -0700

    Create scores.js

---
+++ pages/api/scores.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  try {
+    const r = await fetch(
+      'https://site.api.espn.com/apis/site/v2/sports/golf/leaderboard?event=401580326',
+      { headers: { 'User-Agent': 'Mozilla/5.0' } }
+    );
+    const data = await r.json();
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+}
